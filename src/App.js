e9b800a286d70f29568f4927907bede4aa8fb8eb
Kris Lai <laikxzhi@gmail.com>
2020-08-14 17:55:43 -0700

    testing create react env

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          RELEARNING REACT JS.
         </p>
         <a
           className="App-link"
