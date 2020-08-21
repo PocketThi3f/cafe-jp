cc83390efc289af3eb03937df77915be8cdba59c
Kris Lai <laikxzhi@gmail.com>
2020-08-21 00:18:03 -0700

    changes to app js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </header>
+    <div>
+
     </div>
-  );
+  )
 }
 
 export default App;
