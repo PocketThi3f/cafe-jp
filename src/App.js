378e6361aeb8a861261d636eb4b928429e112729
Kris Lai <laikxzhi@gmail.com>
2020-08-27 23:51:28 -0700

    verified header component is working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Header from './containers/Header/Header';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      
+      <Header></Header>
     )
   }
 }
--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-export class Header extends Component {
+export default class Header extends Component {
     render() {
         return (
             <div>
