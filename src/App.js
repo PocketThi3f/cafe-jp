49ad1501627f1d1a9e916236fccdc975381b5ba0
Kris Lai <laikxzhi@gmail.com>
2020-08-28 19:44:37 -0700

    added image and created panel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import Header from './containers/Header/Header';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <Header></Header>
+      <Header />
     )
   }
 }
---
+++ src/components/ImagePanel/ImagePanel.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ImagePanel extends Component {
+    render() {
+        return (
+            <img src="" alt="Placeholder Image"/>
+        )
+    }
+}
