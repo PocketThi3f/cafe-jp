4a10b51b92170b70c6a322d2ac0afd69445aa0b2
Kris Lai <laikxzhi@gmail.com>
2020-08-31 17:48:45 -0700

    added clock js as component into panel

--- src/containers/ClockPanel/ClockPanel.js
+++ src/containers/ClockPanel/ClockPanel.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './ClockPanel.scss';
 
+// Import Clock file
+import Clock from './Clock/Clock';
+
 
 export default class ClockPanel extends Component {
     render() {
         return (
-            <div className="clock-section">
-                <p>Current Time is:</p>
-                <h3>12:30 PM PST</h3>
-            </div>
+            <Clock />
         )
     }
 }
