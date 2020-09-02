632e59f07c9a0edf47b9d38510a83fa2ca13c6de
Kris Lai <laikxzhi@gmail.com>
2020-09-02 16:32:59 -0700

    changed using ul to div for nav items

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import './Header.scss';
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                <div>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
+            <div className="Header">
+                <div className="nav-bar">
+                    <div className="nav-item">Home</div>
+                    <div className="nav-item">Clock</div>
+                    <div className="nav-item">Ticket Booth</div>
                 </div>
-                <h1>Welcome to Café JP</h1>
-                <p>We are currently:</p>
-                <h2>OPEN!</h2>
+                <div className="Intro">
+                    <h1>Welcome to Café JP</h1>
+                    <p>We are currently:</p>
+                    <h2>OPEN!</h2>
+                </div>
             </div>
         )
     }
