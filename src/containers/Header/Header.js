69bd90a73d3e9ddca682bb5e26727e21d9a7f6eb
Kris Lai <laikxzhi@gmail.com>
2020-09-01 17:02:35 -0700

    added temp nav items to header file

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -4,6 +4,13 @@ export default class Header extends Component {
     render() {
         return (
             <div>
+                <div>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
                 <h1>Welcome to Café JP</h1>
                 <p>We are currently:</p>
                 <h2>OPEN!</h2>
