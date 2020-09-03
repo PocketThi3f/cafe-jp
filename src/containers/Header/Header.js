c27e88b17a01d6c87fa34bcc2d1f62442b2db6d0
Kris Lai <laikxzhi@gmail.com>
2020-09-02 17:20:07 -0700

    edited nav items

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -5,9 +5,9 @@ export default class Header extends Component {
     render() {
         return (
             <div className="Header">
-                <div className="nav-bar">
+                <div className="nav">
                     <div className="nav-item">Home</div>
-                    <div className="nav-item">Clock</div>
+                    <div className="nav-item">Order Status</div>
                     <div className="nav-item">Ticket Booth</div>
                 </div>
                 <div className="Intro">
