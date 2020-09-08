2dcffeb77f5a4731f0db485a7cee1f0188b88ee5
Kris Lai <laikxzhi@gmail.com>
2020-09-08 01:17:50 -0700

    added footer onto page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './containers/Header/Header';
 import ImagePanel from './components/ImagePanel/ImagePanel';
 import Clock from './containers/ClockPanel/ClockPanel';
 import TicketBooth from './containers/TicketBooth/TicketBooth';
+import Footer from './containers/Footer/Footer';
 
 // Sass file for App
 import './App.scss';
@@ -17,6 +18,7 @@ class App extends Component {
         <ImagePanel />
         <Clock />
         <TicketBooth />
+        <Footer />
       </div>
     )
   }
