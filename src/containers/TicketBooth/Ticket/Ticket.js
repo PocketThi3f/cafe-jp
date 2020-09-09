9a2149f8852236bcf0c69e2c1a94f6cd242cbed4
Kris Lai <laikxzhi@gmail.com>
2020-09-08 23:04:14 -0700

    added p tag for eta

--- src/containers/TicketBooth/Ticket/Ticket.js
+++ src/containers/TicketBooth/Ticket/Ticket.js
@@ -8,6 +8,7 @@ export default class Ticket extends Component {
                 <p>This ticket will update you on the status of your order.</p>
                 <p>Approximate wait time...</p>
                 <strong><p>5 minutes</p></strong>
+                <p>ETA:<strong> 12:35 PM PST</strong></p>
             </div>
         )
     }
