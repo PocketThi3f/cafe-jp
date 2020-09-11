15c96ac024c72773eea81be0553ba77fc562c07d
Kris Lai <laikxzhi@gmail.com>
2020-09-11 00:08:06 -0700

    attempting parallax scroll

--- src/components/ImagePanel/ImagePanel.js
+++ src/components/ImagePanel/ImagePanel.js
@@ -5,7 +5,7 @@ import cafeImg from '../../assets/images/city-cafe.jpg';
 export default class ImagePanel extends Component {
     render() {
         return (
-        <div className="cafe-image" >
+        <div className="cafe-image">
             <img src={cafeImg} alt="cafe shop"/>
         </div>
         )
