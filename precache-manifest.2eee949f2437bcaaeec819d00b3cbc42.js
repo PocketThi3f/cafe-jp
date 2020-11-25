54875ebd7b939de74cf9261a266e8ba376f32e21
PocketThi3f <laikxzhi@gmail.com>
2020-11-24 22:03:00 -0800

    Updates

---
+++ precache-manifest.2eee949f2437bcaaeec819d00b3cbc42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2aa86bdbd23f25295e5fe91ef1f41aa",
+    "url": "/cafe-jp/index.html"
+  },
+  {
+    "revision": "2366b09ef4eba59d8b08",
+    "url": "/cafe-jp/static/css/main.7d7aa9f1.chunk.css"
+  },
+  {
+    "revision": "85cb6a4b55fd7a2073b2",
+    "url": "/cafe-jp/static/js/2.b0b7b4f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cafe-jp/static/js/2.b0b7b4f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2366b09ef4eba59d8b08",
+    "url": "/cafe-jp/static/js/main.65357922.chunk.js"
+  },
+  {
+    "revision": "ca0da3408fe40e7044a9",
+    "url": "/cafe-jp/static/js/runtime-main.2c9de972.js"
+  },
+  {
+    "revision": "74328da42d63584c9c9eb2b43f969548",
+    "url": "/cafe-jp/static/media/city-cafe.74328da4.jpg"
+  }
+]);
